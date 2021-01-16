62b7d8a3f59adc3da73bde00a3ffd3bb5280249d
kevyn.schrondweiler <kevyn.schrondweiler@ITS4U144.its4u-group.com>
2021-01-16 15:15:22 +0100

    Consumer with kafka

---
+++ src/main/java/org/boudet/spring/integration/sample/activator/LoggerAnyMessages.java
@@ -0,0 +1,20 @@
+package org.boudet.spring.integration.sample.activator;
+
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component("loggerAnyMessage")
+public class LoggerAnyMessages {
+	
+	@ServiceActivator
+	public void handle(Message<?> message) {
+
+		log.info(Thread.currentThread().getName()+" # "+(String) message.getPayload());
+			
+	}	
+
+}
