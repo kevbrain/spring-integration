854fe13820acb6b97d23573073e6c26c1de12e62
kevyn.schrondweiler <kevyn.schrondweiler@ITS4U144.its4u-group.com>
2021-01-19 18:13:09 +0100

    config

--- src/main/java/org/boudet/spring/integration/sample/Application.java
+++ src/main/java/org/boudet/spring/integration/sample/Application.java
@@ -2,13 +2,13 @@ package org.boudet.spring.integration.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
-@SpringBootApplication
-@Configuration
-//@ImportResource({"classpath*:*-context.xml"})
-@ImportResource({"file:${metrics.context.classpath}*-context.xml"})
+@SpringBootApplication(exclude = JpaRepositoriesAutoConfiguration.class)
+@ImportResource({"classpath*:*-context.xml"})
+//@ImportResource({"file:${metrics.context.classpath}*-context.xml"})
 public class Application {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
